6e0805d936102dfd8bfe747ecc088ccd46b27641
Juan Torres <juan.torres@halopowered.com>
2023-08-08 12:31:04 -0400

    feat(login-screen): add interfaces for user, auth, list, task

---
+++ lib/environments/config.dart
@@ -0,0 +1,12 @@
+class EnvironmentConfig {
+  final String api;
+  // final String mainApi;
+  final bool production;
+
+  EnvironmentConfig({
+    required this.api,
+    // required this.mainApi,
+    required this.production,
+    // ... add other configuration variables
+  });
+}
---
+++ lib/environments/dev.config.dart
@@ -0,0 +1,37 @@
+// import 'dart:io';
+import 'dart:io';
+
+import 'package:todo_flutter/environments/config.dart';
+
+class DevConfig extends EnvironmentConfig {
+  // DevConfig(super.api, super.mainApi, super.production);
+  DevConfig()
+      : super(
+          api: _getApiUrl(),
+          // mainApi: _getMainApiUrl(),
+          production: false,
+          // ... initialize other configuration variables
+        );
+
+  static String _getApiUrl() {
+    if (Platform.isAndroid) {
+      return 'http://10.0.2.2:4000/v1'; // Android emulator
+    } else if (Platform.isIOS) {
+      return 'http://127.0.0.1:4000/v1'; // iOS simulator
+    }
+    // Default to a fallback URL
+    return 'http://localhost:4000/v1';
+  }
+
+  // static String _getMainApiUrl() {
+  //   // Define the main API URL logic similarly as _getApiUrl()
+  //   // You can customize this based on your needs
+  //   if (Platform.isAndroid) {
+  //     return 'http://10.0.2.2:4000/v1'; // Android emulator
+  //   } else if (Platform.isIOS) {
+  //     return 'http://127.0.0.1:4000/v1'; // iOS simulator
+  //   }
+  //   // Default to a fallback URL
+  //   return 'http://localhost:4000/v1';
+  // }
+}
